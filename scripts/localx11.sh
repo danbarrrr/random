3e34cb52eea20b1947c28b0198dacb575f2e888b
danbarrrr <danbarrrr@gmail.com>
2022-04-30 16:21:43 -0400

    android script

--- scripts/localx11.sh
+++ scripts/localx11.sh
@@ -1,7 +1,8 @@
 ####################################################
-######### EXPORT DISPLAY TO LOCAL HOST X11 ######### 
+################### XServer XSDL ###################
+######### EXPORT DISPLAY TO LOCAL HOST X11 #########
 
 export DISPLAY=:0
 export PULSE_DISPLAY=tcp:localhost:4713
-export XDG_RUNTIME_DIR=/
+export XDG_RUNTIME_DIR= 
 export RUNLEVEL=3
