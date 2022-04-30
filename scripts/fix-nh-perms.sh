1dd27a9a1e6875d88f266f507ccf5ba09a9c08cf
danbarrrr <danbarrrr@gmail.com>
2022-04-30 16:16:19 -0400

    android script

---
+++ scripts/fix-nh-perms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pm grant com.offsec.nethunter android.permission.ACCESS_FINE_LOCATION 
+
+pm grant com.offsec.nethunter android.permission.ACCESS_COARSE_LOCATION
+
+pm grant com.offsec.nethunter android.permission.READ_EXTERNAL_STORAGE
+
+pm grant com.offsec.nethunter android.permission.WRITE_EXTERNAL_STORAGE
+
+pm grant com.offsec.nethunter com.offsec.nhterm.permission.RUN_SCRIPT 
+
+pm grant com.offsec.nethunter com.offsec.nhterm.permission.RUN_SCRIPT_SU
+
+pm grant com.offsec.nethunter com.offsec.nhterm.permission.RUN_SCRIPT_NH
+
+pm grant com.offsec.nethunter com.offsec.nhterm.permission.RUN_SCRIPT_NH_LOGIN
+
+echo "COMPLETE"
---
+++ scripts/localx11.sh
@@ -0,0 +1,7 @@
+####################################################
+######### EXPORT DISPLAY TO LOCAL HOST X11 ######### 
+
+export DISPLAY=:0
+export PULSE_DISPLAY=tcp:localhost:4713
+export XDG_RUNTIME_DIR=/
+export RUNLEVEL=3
