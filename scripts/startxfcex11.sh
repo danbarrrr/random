d7768538ae915e225d50f115d674670f7b113b6c
danbarrrr <danbarrrr@gmail.com>
2022-04-30 22:31:39 -0400

    updates/add comments

--- scripts/startxfcex11.sh
+++ scripts/startxfcex11.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ####################################################
-######### EXPORT DISPLAY TO LOCAL HOST X11 #########
+########## START XFCE4 ON LOCAL HOST X11 ###########
 ################### XServer XSDL ###################
 
 export DISPLAY=:0
@@ -8,8 +8,11 @@ export PULSE_DISPLAY=tcp:localhost:4713
 export XDG_RUNTIME_DIR=/data/local/tmp/runtime-root
 export RUNLEVEL=3
 
+xfce4-session
+
 ### FOR CHROOT ON ANDROID ###
 ## MUST START XServer XSDL ##
-## NOT WORKING ? 
-## USE COMMAND source localx11.sh
-## THEN x-window-manager & firefox (or whatever GUI app)
+#### THEN EXECUTE SCRIPT ####
+## NOT WORKING ? CRASHING ? 
+## UNINSTALL POWER MANAGER FROM CHROOT
+## apt remove xfce4-power-manager
